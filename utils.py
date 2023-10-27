b730a71153d9fc1c949ab5f985a77f2d810bf5b9
Veeti Hytönen <veeti.hytonen@gmail.com>
2023-10-27 14:27:11 +0300

    removed testing settings from util

--- utils.py
+++ utils.py
@@ -1,15 +1,12 @@
 from flask import request, session, abort
 
 def check_csrf() -> None:
-    # if session['csrf_token'] != request.form['csrf_token']:
-    #     abort(403)
-    return
+    if session['csrf_token'] != request.form['csrf_token']:
+        abort(403)
 
 def check_auth(access_level: int) -> tuple[bool, str | None]:
-    # if 'username' not in session:
-    #     return False, 'Et ole kirjautunut sisään'
+    if 'username' not in session:
+        return False, 'Et ole kirjautunut sisään'
         
-    # if session['role'] < access_level:
-    #     return False, 'Käyttäjälläsi ei ole oikeutta tähän toimintoon'
-    
-    return True, None
+    if session['role'] < access_level:
+        return False, 'Käyttäjälläsi ei ole oikeutta tähän toimintoon'
